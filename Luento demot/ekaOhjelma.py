6bf491593a0ce4ef29da4398a6819473b6e46b19
Kimmo Lehtimaki <kimmo.lehtimaki@student.laurea.fi>
2023-04-18 04:11:14 +0300

    uusi nimeäminen

---
+++ Luento demot/ekaOhjelma.py
@@ -0,0 +1,7 @@
+# Tämä on ensimmäinen ohjelmani
+
+print("Welcome to Introduction to Programming!")
+print("First we will practice using the print command.")
+print("This program prints three lines of text on the screen.")
+
+print("Thanks for reading!")  # Tämä on myös kommentti
